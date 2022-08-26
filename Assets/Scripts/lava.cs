34cbaf82f1800cfa3dd02b7160020d16ce6cb711
AlwalidX <chnl.secret.email@gmail.com>
2022-08-27 02:45:38 +0400

    added 2 more levels and half fallimg spikes

--- Assets/Scripts/lava.cs
+++ Assets/Scripts/lava.cs
@@ -1,9 +1,8 @@
-using UnityEngine.SceneManagement;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spikes : MonoBehaviour
+public class lava : MonoBehaviour
 {
     void  OnCollisionEnter2D(Collision2D other)
     {
